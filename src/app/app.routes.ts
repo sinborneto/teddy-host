6dac35a70d46974ca8e02ce9657d9fbc2d991f17
Sinval Borges <sinborneto@yahoo.com.br>
2025-03-07 22:57:56 -0300

    initializing project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
+import { loadRemoteModule } from '@angular-architects/native-federation';
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    loadComponent: () => loadRemoteModule('login' , './Component')
+    .then((m) => m.LoginComponent)
+  }
+];
